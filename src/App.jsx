ed0dcb0ab7f3d3161c74ae7ecc9f7beab8069cbc
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-05-24 13:02:07 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "../components/NavBar";
+function App() {
+  return (
+    <div className="container">
+      <NavBar />
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
