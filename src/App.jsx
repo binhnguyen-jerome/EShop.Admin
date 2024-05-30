513918e79db97e5ca4c82ffd5e0c28750a3ca569
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-05-30 16:49:58 +0700

    add not found pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProductForm from "./pages/Product/ProductForm";
+import ProductDetail from "./pages/Product/ProductDetail";
+import NotFound from "./components/NotFound";
 function App() {
   const mode = useSelector((state) => state.theme.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -24,6 +26,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
+            <Route path="*" element={<NotFound />} />
             <Route element={<PrivateRoute />}>
               <Route element={<Layout />}>
                 <Route path="/" element={<Dashboard />} />
@@ -31,6 +34,7 @@ function App() {
                 <Route path="/products" element={<ProductList />} />
                 <Route path="/products/edit/:id" element={<ProductForm />} />
                 <Route path="/products/create" element={<ProductForm />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
                 <Route path="/categories" element={<CategoryList />} />
                 <Route path="/categories/edit/:id" element={<CategoryForm />} />
                 <Route path="/categories/create" element={<CategoryForm />} />
